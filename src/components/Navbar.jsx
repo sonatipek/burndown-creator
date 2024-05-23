0925fd52c5f5043e448ae24e9c26e5771fca6023
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-05-23 17:28:07 +0300

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { Select, SelectItem } from "@tremor/react"; //navbar como için
+import { useEffect, useState } from "react";
+import i18n from "i18next";
+import { RiGlobalLine } from "@remixicon/react";
+
+export default function Navbar() {
+  const [languageValue, setLanguageValue] = useState(
+    localStorage.getItem("languagePreference"),
+  );
+
+  useEffect(() => {
+    i18n.changeLanguage(languageValue);
+    localStorage.setItem("languagePreference", languageValue);
+  }, [languageValue]);
+
+  return (
+    <div className="relative shadow-tremor-input shadow-tremor-brand-muted">
+      <div className="container mx-auto flex items-center justify-between py-5">
+        <a
+          href="/"
+          className="text-tremor-title font-extrabold text-tremor-brand"
+        >
+          Burndown Creator
+        </a>
+
+        <div>
+          <Select
+            id="language"
+            name="language"
+            value={languageValue}
+            onValueChange={setLanguageValue}
+            className="w-1/12"
+            icon={RiGlobalLine}
+          >
+            <SelectItem value="tr">Türkçe</SelectItem>
+            <SelectItem value="en">English</SelectItem>
+          </Select>
+        </div>
+      </div>
+    </div>
+  );
+}
