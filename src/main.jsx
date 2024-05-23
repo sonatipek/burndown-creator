2ed28ef178798ca200e69b9f899bc2664911ef2f
Sonat Saygın İPEK <sonatipek55@gmail.com>
2024-05-23 17:33:57 +0300

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Footer from './components/Footer.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Footer from "./components/Footer.jsx";
+import Navbar from "./components/Navbar.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Navbar />
     <App />
     <Footer />
   </React.StrictMode>,
-)
+);
